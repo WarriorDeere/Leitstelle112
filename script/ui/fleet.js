6d577afa9caa7ca9bbf0815016d37aa996af28b2
Warrior Deere <warriorcatjohndeere@protonmail.com>
2023-06-29 15:46:16 +0200

    improved provided information within the shop
    40% of #19

--- script/ui/fleet.js
+++ script/ui/fleet.js
@@ -42,17 +42,82 @@ export const fleet = new class {
         const vehicleArray = await shopItemData.then((r) => { return r.vehicle })
         content.innerHTML = '';
 
-        vehicleArray.forEach(vehicle => {
+        vehicleArray.forEach(async vehicle => {
             const vehicleCardBone = document.createElement('div');
             const vehicleCardUUID = crypto.randomUUID();
             content.appendChild(vehicleCardBone);
             vehicleCardBone.id = vehicleCardUUID;
-            vehicleCardBone.innerHTML = JSON.stringify(vehicle);
+            vehicleCardBone.classList.add('vehicle-card');
+
+            const vehicleType = String(vehicle.type);
+
+            vehicleCardBone.innerHTML = `
+            <div class="vc-type">${vehicleType}</div>
+            <div class="vc-crew" id="crew-member-area-${vehicleCardUUID}">Mögliche Aufteilung: </div>
+            <div class="vc-inventory" id="inventory-area-${vehicleCardUUID}"></div>
+            <div class="vc-interact"></div>
+            `;
+
+            const crewCardContainer = document.querySelector(`#crew-member-area-${vehicleCardUUID}`);
+
+            const crewStrengthCardBone = document.createElement('div');
+            crewStrengthCardBone.innerHTML = `Personal: zwischen ${Number(vehicle.crew_min)} und ${Number(vehicle.crew_max)}`;
+            crewCardContainer.appendChild(crewStrengthCardBone);
+
+            vehicle.crew.forEach(crewMember => {
+                const crewMemberUUID = crypto.randomUUID();
+                const crewMemberCardBone = document.createElement('div');
+
+                crewMemberCardBone.classList.add('crew-meber-card');
+                if (crewMember.required) {
+                    crewMemberCardBone.classList.add('crew-meber-req');
+                }
+                crewMemberCardBone.id = `crew-member-${crewMemberUUID}`
+
+                crewMemberCardBone.innerHTML = String(crewMember.type_long);
+
+                crewCardContainer.appendChild(crewMemberCardBone);
+            });
+
+            const inventoryCardContainer = document.querySelector(`#inventory-area-${vehicleCardUUID}`);
+            
+            vehicle.equipment.vehicle_specific.forEach(kit => {
+                const kitUUID = crypto.randomUUID();
+                const equipmentKitBone = document.createElement('div');
+
+                equipmentKitBone.classList.add('equipment-kit-card');
+                equipmentKitBone.id = `kit-${kitUUID}`;
+
+                if (kit === undefined) {
+                    equipmentKitBone.innerHTML = 'Keine Ausrüstung verfügbar';
+                }
+                else {
+                    equipmentKitBone.innerHTML = `Zusätzliche Ausrüstung: ${String(kit)}`;
+                }
+
+                inventoryCardContainer.appendChild(equipmentKitBone);
+            });
+
+            if (vehicle.equipment.default) {
+                const defaultKitUUID = crypto.randomUUID();
+                const defaultKitBone = document.createElement('div');
+
+                defaultKitBone.classList.add('equipment-kit-card');
+                defaultKitBone.id = `default-kit-${defaultKitUUID}`;
+
+                inventoryCardContainer.appendChild(defaultKitBone);
+                if (vehicle.equipment.default.hose) {
+                    const hoseTextDOM = document.createTextNode(`Schläuche: ${Number(vehicle.equipment.default.hose)}m`);
+                    defaultKitBone.appendChild(hoseTextDOM);
+                }
+                if (vehicle.equipment.default.scba) {
+                    const scbaTextDOM = document.createTextNode(`Atemschutzgeräte: ${Number(vehicle.equipment.default.scba)}`);
+                    defaultKitBone.appendChild(scbaTextDOM);
+                }
+            }
         });
 
         const dialogTitle = document.querySelector(`#${identifier.title}`);
-        dialogTitle.innerHTML = `
-            Fahrzeugmarkt
-        `;
+        dialogTitle.innerHTML = `Fahrzeugmarkt`;
     }
 }
